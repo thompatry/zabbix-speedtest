bdf45aec313688c0b9640027beeb830b266d8506
Thomas Patry <thompatry@gmail.com>
2019-02-14 21:35:31 -0600

    Overhaul to use a bugless version of Speedtest-cli

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # Zabbix Speedtest template
+# Tested on Zabbix 4.0.3
 
 ## Installation (Generic x86_64)
-
-- Copy the speedtest binary (`bin/speedtest-linux-amd64`) and `speedtest.sh` to `/etc/zabbix/bin`
-- Make them both executable: `chmod +x /etc/zabbix/bin/speedtest /etc/zabbix/bin/speedtest.sh`
-- Install the systemd service and timer: `cp speedtest.service speedtest.timer /etc/systemd/system`
+- Install python-pip: `sudo apt install python-pip`
+- Install speedtest: `pip install speedtest-cli`
+- Copy `speedtest.sh` to `/etc/zabbix/bin`: `sudo cp speedtest.sh /etc/zabbix/bin`
+- Make it executable: `sudo chmod a+x /etc/zabbix/bin/speedtest.sh`
+- Install the systemd service and timer: `sudo cp speedtest.service /etc/systemd/system` and `sudo cp speedtest.timer /etc/systemd/system`
 - Start and enable the timer: `systemctl enable --now speedtest.timer`
-- Import the zabbix-agent config: `cp speedtest.conf /etc/zabbix/zabbix_agentd.conf.d`
+- Copy speedtest.conf into /etc/zabbix/zabbix-agentd.d: `sudo cp speedtest.conf /etc/zabbix/zabbix_agentd.d`
 - Restart zabbix-agent: `sudo systemctl restart zabbix-agent`
 - Import `template_speedtest.xml` on your Zabbix server
-
-## Installation (OpenWRT)
-
-- Copy the speedtest binary (`bin/speedtest-linux-arm-static`) and `speedtest.sh` to `/etc/zabbix_agentd.conf.d/bin`
-- Make them both executable: `chmod +x /etc/zabbix_agentd.conf.d/bin/speedtest /etc/zabbix_agentd.conf.d/bin/speedtest.sh`
-- Import the zabbix-agent config: `cp speedtest.conf /etc/zabbix_agentd.conf.d`
-- Restart zabbix-agent: `/etc/init.d/zabbix-agentd restart`
-- Install the cron job: `crontab -e` -> Add the content of `speedtest.crontab`
-- Import `template_speedtest.xml` on your Zabbix server
+- Add template to server that has the speedtest.sh service on in Zabbix
--- speedtest.sh
+++ speedtest.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CACHE_FILE=/tmp/speedtest.log
+CACHE_FILE=/var/log/zabbix/speedtest.log
 LOCK_FILE=/tmp/speedtest.lock
 
 run_speedtest() {
@@ -20,34 +20,39 @@ run_speedtest() {
     local output date location_id location ping download upload
     local download_mb upload_mb
 
-    output=$(./speedtest -r)
+    output=$(python /usr/local/bin/speedtest --server 20531 --csv --csv-delimiter '|')
 
     # Debug
-    # output='2017-09-22 09:15:02 +0000|4997|"inexio (Saarlouis, Germany)"|30.30|82121|19392'
+    # new output=6889|Twin Valley Communications|Miltonvale, KS|2019-02-15T00:30:00.344076Z|76.63434604957892|26.276|222487444.93646407|32353150.5438985||70.179.147.69
+    # old output='2017-09-22 09:15:02 +0000|4997|"inexio (Saarlouis, Germany)"|30.30|82121|19392'
     # sleep 10
 
     echo "Output: $output"
 
     # Extract fields
-    date=$(echo "$output" | cut -f1 -d '|')
-    location_id=$(echo "$output" | cut -f2 -d '|')
-    location=$(echo "$output" | cut -f3 -d '|' | sed 's/^"\(.*\)"$/\1/g')
-    ping=$(echo "$output" | cut -f4 -d '|')
-    download=$(echo "$output" | cut -f5 -d '|')
-    upload=$(echo "$output" | cut -f6 -d '|')
-
+    date=$(echo "$output" | cut -f4 -d '|')
+    location_id=$(echo "$output" | cut -f1 -d '|')
+    location=$(echo "$output" | cut -f2 -d '|' | sed 's/^"\(.*\)"$/\1/g')
+    ping=$(echo "$output" | cut -f6 -d '|')
+    download=$(echo "$output" | cut -f7 -d '|')
+    upload=$(echo "$output" | cut -f8 -d '|')
+    distance=$(echo "$output" | cut -f5 -d '|')
+    local_ip=$(echo "$output" | cut -f10 -d '|')
+    city=$(echo "$output" | cut -f3 -d '|')
     # Convert to MBit/s
-    download_mb=$(echo "$download" | awk '{ printf("%.2f\n", $1 / 1024) }')
-    upload_mb=$(echo "$upload" | awk '{ printf("%.2f\n", $1 / 1024) }')
+    download_mb=$(echo "$download" | awk '{ printf("%.2f\n", $1 / 1048576) }')
+    upload_mb=$(echo "$upload" | awk '{ printf("%.2f\n", $1 / 1048576) }')
 
     {
         echo "Date: $date"
-        echo "Server: $location [${location_id}]"
+        echo "Server: $location - $city[${location_id}]"
+        echo "Distance: $distance"
         echo "Ping: $ping ms"
         echo "Download: $download bit/s"
         echo "Upload: $upload bit/s"
         echo "Download (MB): $download_mb Mbit/s"
         echo "Upload (MB): $upload_mb Mbit/s"
+        echo "Local IP: $local_ip"
     } > "$CACHE_FILE"
 
     # Make sure to remove the lock file (may be redundant)
@@ -66,6 +71,9 @@ case "$1" in
         ;;
     -p|--ping)
         awk '/Ping/ { print $2 }' "$CACHE_FILE"
+        ;;
+    -s|--server)
+        awk '/Server/ { print }' "$CACHE_FILE"
         ;;
     -f|--force)
         rm -rf "$LOCK_FILE"
